50e0c46f52034bd47d543d530300eb7136eed5b1
bsoutham <bsoutham@gmail.com>
2019-12-17 15:39:57 -0700

    add linux build for circleci

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+DT=`date +%F`
+GC=`git rev-parse --short HEAD`
+RELEASE_PKG=github.com/HewlettPackard/hpecli/pkg/version
+
+go build -o hpecli -ldflags "-X '$RELEASE_PKG.version=v0.0.1' -X '$RELEASE_PKG.builtAt=$DT' -X '$RELEASE_PKG.gitCommit=$GC'" ./cmd/hpecli
