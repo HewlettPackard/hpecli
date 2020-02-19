b0456fcc589240fd09c0ca3e66aadbeed4a9c170
bsoutham <bsoutham@gmail.com>
2020-02-18 18:01:44 -0700

    add self-signed function in unit test

--- pkg/internal/rest/requests_test.go
+++ pkg/internal/rest/requests_test.go
@@ -32,13 +32,15 @@ func TestOptionsExecuted(t *testing.T) {
 
 	fn3 := AddHeaders("someKey", "someValue")
 
+	fn4 := AllowSelfSignedCerts()
+
 	ts := newTestServer("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
 
 	defer ts.Close()
 
-	r, err := Get(ts.URL, fn1, fn2, fn3)
+	r, err := Get(ts.URL, fn1, fn2, fn3, fn4)
 	if err != nil {
 		t.Fatal(err)
 	}
