23ccee560185826dd4a06022b3c6b0800aa5718d
bsoutham <bsoutham@users.noreply.github.com>
2020-03-11 22:36:19 -0600

    testing for read passwords

--- internal/platform/password/read.go
+++ internal/platform/password/read.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 	"syscall"
@@ -11,32 +12,60 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+// These are defined here for testing and should be
+// needed outside of that
+var (
+	stdout io.Writer
+	stdin  io.Reader
+	reader passwordReader
+)
+
+// interface to allow us to test reading from the console
+type passwordReader interface {
+	readPassword() (string, error)
+}
+
+type stdInPasswordReader struct{}
+
+func init() {
+	stdout = os.Stdout
+	stdin = os.Stdin
+	reader = &stdInPasswordReader{}
+}
+
+// readPassword reads password from stdin
+func (pr stdInPasswordReader) readPassword() (string, error) {
+	//nolint:unconvert  // false positive lint err
+	pwd, err := terminal.ReadPassword(int(syscall.Stdin))
+
+	return string(pwd), err
+}
+
 // ReadFromConsole write the prompt to the console and then
 // will read the password from the user console without
 // echoing the password to the console
 func ReadFromConsole(prompt string) (string, error) {
-	fmt.Fprint(os.Stdout, prompt)
+	fmt.Fprint(stdout, prompt)
 
-	//nolint:unconvert  // false positive lint err
-	p, err := terminal.ReadPassword(int(syscall.Stdin))
+	p, err := reader.readPassword()
 	if err != nil {
 		return "", err
 	}
 
 	// rather than force everyone to do a linefeed after reading
 	// we'll just do it here
-	fmt.Fprint(os.Stdout, "\n")
+	fmt.Fprint(stdout, "\n")
 
-	return string(p), nil
+	return p, nil
 }
 
-// ReadFromStdIn reads from os.Stdin and trims any  CR & LF characters
+// ReadFromStdIn reads from os.Stdin and trims any CR & LF characters
 // as well as any spaces around the input.  This method allows passwords
 // to be passed to the CLI with commands like:
 //   <Windows>
 //     echo mypassword | hpecli ilo login --host 1.1.1.1 -u admin --password-stdin
 func ReadFromStdIn() (string, error) {
-	reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(stdin)
 
 	t, err := reader.ReadString('\n')
 	if err != nil {
@@ -48,8 +77,14 @@ func ReadFromStdIn() (string, error) {
 	return t, nil
 }
 
-func Read(password *string, promptFor bool, prompt string) (err error) {
-	if *password != "" && promptFor {
+// Read will use one of several methods to get a password from a user
+// password is an existing password if set.  This is return unmodified if
+// passed in with a value.
+// readStdIn is true if the password should be read from standard in
+// prompt is what the console prompt should be if it should be read
+// from the console from the user
+func Read(password *string, readStdIn bool, prompt string) (err error) {
+	if *password != "" && readStdIn {
 		return errors.New("--password and --password-stdin are mutually exclusive")
 	}
 
@@ -59,7 +94,7 @@ func Read(password *string, promptFor bool, prompt string) (err error) {
 	}
 
 	// asked to read from stdin
-	if promptFor {
+	if readStdIn {
 		*password, err = ReadFromStdIn()
 		return
 	}
