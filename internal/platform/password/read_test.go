8440d49a9d72f5945bca1cdd11c351201d67d1c7
Blaine R Southam <bsoutham@users.noreply.github.com>
2020-03-11 22:49:20 -0600

    bsoutham/test passwords (#84)
    * clean login commands
    
    * testing for read passwords

--- internal/platform/password/read_test.go
+++ internal/platform/password/read_test.go
@@ -3,6 +3,8 @@
 package password
 
 import (
+	"bytes"
+	"errors"
 	"strings"
 	"testing"
 )
@@ -32,4 +34,124 @@ func TestPasswordAlreadySpecified(t *testing.T) {
 	if got != p {
 		t.Error("error: password unexpectedly modified")
 	}
+}
+
+func TestReadFromConsoleWithError(t *testing.T) {
+	out := &bytes.Buffer{}
+	stdout = out
+	reader = &fakePasswordReader{ReturnError: true}
+
+	want := "myPrompt:"
+	_, err := ReadFromConsole(want)
+
+	if err == nil {
+		t.Fatal("should have seen an error")
+	}
+
+	got := out.String()
+	if got != want {
+		t.Fatalf("didn't get expected prompt written on out.  got=%s, want=%s", got, want)
+	}
+}
+
+func TestReadFromConsole(t *testing.T) {
+	in := &bytes.Buffer{}
+	out := &bytes.Buffer{}
+	stdin = in
+	stdout = out
+	reader = &fakePasswordReader{Password: "mypassword"}
+
+	got, err := ReadFromConsole("abc")
+	if err != nil {
+		t.Fatalf("unexpected error: %+v", err)
+	}
+
+	if out.String() != "abc\n" {
+		t.Error("output should have ended with a \\n")
+	}
+
+	if got != "mypassword" {
+		t.Fatal("didn't read passsword correctly")
+	}
+}
+
+func TestReadFromStdin(t *testing.T) {
+	in := bytes.NewBufferString(" mypassword \n ")
+	stdin = in
+
+	got, err := ReadFromStdIn()
+	if err != nil {
+		t.Fatalf("unexpected error: %+v", err)
+	}
+
+	// ensure password is read.. and trimmed
+	if got != "mypassword" {
+		t.Fatal("didn't read passsword correctly")
+	}
+}
+
+func TestReadFromStdinOnError(t *testing.T) {
+	// not putting a \n will cause EOF on read
+	in := bytes.NewBufferString("mypassword")
+	stdin = in
+
+	_, err := ReadFromStdIn()
+	if err == nil {
+		t.Fatal("expected EOF error on read without \\n")
+	}
+}
+
+func TestReadPromptFor(t *testing.T) {
+	in := bytes.NewBufferString(" otherPassword \n ")
+	stdin = in
+
+	var got string
+	err := Read(&got, true, "")
+
+	if err != nil {
+		t.Fatalf("unexpected error: %+v", err)
+	}
+
+	// ensure password is read.. and trimmed
+	if got != "otherPassword" {
+		t.Fatal("didn't read passsword correctly")
+	}
+}
+
+func TestReadPrompt(t *testing.T) {
+	in := &bytes.Buffer{}
+	out := &bytes.Buffer{}
+	stdin = in
+	stdout = out
+	reader = &fakePasswordReader{Password: "diff-password"}
+
+	wantPrompt := "enter password: "
+
+	var got string
+	err := Read(&got, false, wantPrompt)
+
+	if err != nil {
+		t.Fatalf("unexpected error: %+v", err)
+	}
+
+	if out.String() != wantPrompt+"\n" {
+		t.Error("output should have ended with a \\n")
+	}
+
+	if got != "diff-password" {
+		t.Fatal("didn't read passsword correctly")
+	}
+}
+
+type fakePasswordReader struct {
+	Password    string
+	ReturnError bool
+}
+
+func (pr fakePasswordReader) readPassword() (string, error) {
+	if pr.ReturnError {
+		return "", errors.New("expected stubbed error")
+	}
+
+	return pr.Password, nil
 }
