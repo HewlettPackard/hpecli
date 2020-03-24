cadc9d687b375e622c9b786be6e3941673d754dc
pramod sareddy <reddypramod85>
2020-03-24 01:28:52 -0500

    :sparkles:autocompletion for zsh and powershell

--- pkg/autocomplete/autocomplete.go
+++ pkg/autocomplete/autocomplete.go
@@ -3,8 +3,6 @@
 package autocomplete
 
 import (
-	"errors"
-
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -15,46 +13,72 @@ type autocompleteOptions struct {
 	acType string
 }
 
+// NewAutoCompleteCommand to generate shell completion scripts for hpecli
 func NewAutoCompleteCommand() *cobra.Command {
 	var opts autocompleteOptions
 
 	var cmd = &cobra.Command{
 		Use:   "autocompletion",
-		Short: "Generates bash completion scripts",
+		Short: "Generates a shell completion scripts for hpecli",
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			return runAutoComplete(cmd, &opts)
 		},
-		Long: `Generates a shell autocompletion script for hpecli.
+		Long: `Generates a shell completion scripts for hpecli.
 
-	NOTE: The current version supports Bash only.
-		  This should work for *nix systems with Bash installed.
+	NOTE: The current version supports Bash, ZShell, and PowerShell.
 
-	By default, the autocomplete.sh file will be generated in current folder
+	By default, the sh|zsh|ps file(s) will be generated in current folder
 
 	Add ` + "`--completionfile=/path/to/file`" + ` flag to set alternative
 	file-path and name.
 
-	Add the autocomplete.sh content to user ~/bash_profile file and reload the terminal`,
+	Add the generated autocomplete.sh content to user ~/bash_profile file and reload the terminal
+	or just source the .sh file directly:`,
 	}
 
-	cmd.PersistentFlags().StringVarP(&opts.targetFile, "completionfile",
-		"", "autocomplete.sh", "autocompletion file")
-	cmd.PersistentFlags().StringVarP(&opts.acType, "type",
-		"", "bash", "autocompletion type (currently only bash supported)")
+	cmd.Flags().StringVarP(&opts.targetFile, "completionfile",
+		"c", "", "autocompletion file")
+	cmd.Flags().StringVarP(&opts.acType, "type",
+		"t", "bash", "autocompletion shell type: {bash|zsh|powershell}")
+
+	_ = cmd.MarkFlagRequired("type")
 
 	return cmd
 }
-
 func runAutoComplete(cmd *cobra.Command, opts *autocompleteOptions) error {
-	if opts.acType != "bash" {
-		return errors.New("bash is the only currently supported shell type")
+	switch opts.acType {
+	case "bash":
+		if opts.targetFile == "" {
+			opts.targetFile = "autocomplete.sh"
+		}
+
+		if err := cmd.Root().GenBashCompletionFile(opts.targetFile); err != nil {
+			return err
+		}
+	case "zsh":
+		if opts.targetFile == "" {
+			opts.targetFile = "autocomplete.zsh"
+		}
+
+		if err := cmd.Root().GenZshCompletionFile(opts.targetFile); err != nil {
+			return err
+		}
+	case "powershell":
+		if opts.targetFile == "" {
+			opts.targetFile = "autocomplete.ps1"
+		}
+
+		if err := cmd.Root().GenPowerShellCompletionFile(opts.targetFile); err != nil {
+			return err
+		}
+	default:
+		return logrus.Errorf("unsupported shell type %s", opts.acType)
 	}
 
-	if err := cmd.Root().GenBashCompletionFile(opts.targetFile); err != nil {
-		return err
-	}
-
-	logrus.Debug("Bash completion file for hpecli saved to", opts.targetFile)
+	logrus.Debugf("%s completion file for hpecli saved to %s", opts.acType, opts.targetFile)
+	logrus.Printf(`%s completion file for hpecli saved in current folder as %s
+	Add the generated file content to user ~/bash_profile file and reload the terminal or 
+	just source the file directly`, opts.acType, opts.targetFile)
 
 	return nil
 }
