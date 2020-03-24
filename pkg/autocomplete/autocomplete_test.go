a6543d20d5dc718f27bb33f331cbb495f8ec6812
pramod reddy <reddypramod85@gmail.com>
2020-03-24 08:30:03 -0500

    autocompletion for zsh and powershell (#117)
    * :sparkles:autocompletion for zsh and powershell
    
    * :bug:fix build fail
    
    Co-authored-by: pramod sareddy <reddypramod85>

--- pkg/autocomplete/autocomplete_test.go
+++ pkg/autocomplete/autocomplete_test.go
@@ -6,6 +6,7 @@ import (
 
 func TestCompletionForBash(t *testing.T) {
 	cmd := NewAutoCompleteCommand()
+	cmd.SetArgs([]string{"--type", "bash"})
 
 	err := cmd.Execute()
 	if err != nil {
@@ -13,13 +14,62 @@ func TestCompletionForBash(t *testing.T) {
 	}
 }
 
-func TestCompletionForNotBash(t *testing.T) {
+func TestCompletionForZsh(t *testing.T) {
+	cmd := NewAutoCompleteCommand()
+	cmd.SetArgs([]string{"--type", "zsh"})
+
+	err := cmd.Execute()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCompletionForPowershell(t *testing.T) {
+	cmd := NewAutoCompleteCommand()
+	cmd.SetArgs([]string{"--type", "powershell"})
+
+	err := cmd.Execute()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+func TestCompletionForNotBashZshPowershell(t *testing.T) {
 	opts := &autocompleteOptions{
-		acType: "zsh",
+		acType: "pwr",
 	}
 
 	err := runAutoComplete(nil, opts)
 	if err == nil {
 		t.Fatal("expected to find error on non bash shell type")
+	}
+}
+
+func TestCompletionForInvalidFIlePathPS(t *testing.T) {
+	cmd := NewAutoCompleteCommand()
+	cmd.SetArgs([]string{"--type", "powershell", "--completionfile", "/"})
+
+	err := cmd.Execute()
+	if err == nil {
+		t.Fatal("expected to find error on invalid file path")
+	}
+}
+
+func TestCompletionForInvalidFIlePathBash(t *testing.T) {
+	cmd := NewAutoCompleteCommand()
+	cmd.SetArgs([]string{"--type", "bash", "--completionfile", "/"})
+
+	err := cmd.Execute()
+	if err == nil {
+		t.Fatal("expected to find error on invalid file path")
+	}
+}
+
+func TestCompletionForInvalidFIlePathZShell(t *testing.T) {
+	cmd := NewAutoCompleteCommand()
+	cmd.SetArgs([]string{"--type", "zsh", "--completionfile", "/"})
+
+	err := cmd.Execute()
+	if err == nil {
+		t.Fatal("expected to find error on invalid file path")
 	}
 }
