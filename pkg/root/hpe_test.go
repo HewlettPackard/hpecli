3c121454c7b327e28f968c81bc22ec58abbf53dc
didou06 <didier.lalli@laposte.net>
2020-08-31 18:33:41 +0200

    Adding test

---
+++ pkg/root/hpe_test.go
@@ -0,0 +1,19 @@
+// (C) Copyright 2019 Hewlett Packard Enterprise Development LP.
+
+package root
+
+import (
+	"testing"
+)
+
+const incorrectValueError = "incorrect value: got=%v, want=%v"
+
+func TestIsDebugOn(t *testing.T) {
+	
+	}
+
+
+func TestIsDebugHTTPOn(t *testing.T) {
+
+}
+
