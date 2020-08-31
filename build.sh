b4763ee45976750b25205984932c5264ae333ea1
didou06 <didier.lalli@laposte.net>
2020-08-31 18:20:58 +0200

    Retrofit changes from Travis on root

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 ISO_DATE=`date +%F`
 GIT_COMMIT=`git rev-parse --short HEAD`
 
-go build -o hpe -ldflags "-X main.sematicVer=0.0.1 -X main.buildDate=$ISO_DATE -X main.gitCommitID=$GIT_COMMIT" ./cmd/hpecli
+go build -o hpe -ldflags "-X main.semanticVer=0.0.1 -X main.buildDate=$ISO_DATE -X main.gitCommitID=$GIT_COMMIT" ./cmd/hpecli
